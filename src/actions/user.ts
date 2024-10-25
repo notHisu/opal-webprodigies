45e3ce87ac1cd75ce651fc445939827e01e28a13
Le Trung Hoa Hieu <nothisu.dev@gmail.com>
2024-10-25 16:06:42 +0700

    feat: add prisma schema

---
+++ src/actions/user.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+export const onAuthenticateUser = async () => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return { status: 403 };
+    }
+
+    const existingUser = await db.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+    });
+  } catch (error) {}
+};
